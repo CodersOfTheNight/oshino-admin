03ed7cdf5969b49c9c1cfcc78bddedfb85cee52c
Sarunas Navickas <sarunasnavickas@Sarunass-MacBook-Pro.local>
2017-08-22 09:22:24 +0300

    Initial structure

---
+++ oshino_admin/version.py
@@ -0,0 +1,18 @@
+"""
+Module just to print out current version.
+That's all.
+"""
+
+VERSION = (0, 1, "0")
+
+
+def get_version():
+    """
+    Returns version
+    """
+    return ".".join(map(str, VERSION))
+
+
+if __name__ == '__main__':
+    print(get_version())
+
---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from setuptools import setup
+from pip.req import parse_requirements
+
+from oshino_admin.version import get_version
+
+install_reqs = list(parse_requirements("requirements/release.txt", session={}))
+test_reqs = list(parse_requirements("requirements/test.txt", session={}))
+
+setup(name="oshino-admin",
+      version=get_version(),
+      description="",
+      author="zaibacu",
+      author_email="zaibacu@gmail.com",
+      packages=["oshino_admin"],
+      install_requires=[str(ir.req) for ir in install_reqs],
+      test_suite="pytest",
+      tests_require=[str(tr.req) for tr in test_reqs],
+      setup_requires=["pytest-runner"],
+      entry_points={'console_scripts': ['oshino-admin = oshino-admin.run:main'
+                                        ]
+                    }
+)
