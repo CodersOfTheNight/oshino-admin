4ca207458b1007fbef11d62f63af9fdf8f91a19b
Šarūnas Navickas <sarunas.navickas@tokenmill.lt>
2018-04-22 21:59:06 +0300

    Fix req parsing

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 from setuptools import setup
-from pip.req import parse_requirements
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 from oshino_admin.version import get_version
 
