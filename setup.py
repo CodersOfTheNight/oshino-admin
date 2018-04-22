fb636fab45ff0872ec55a519fa2c48cf30d9c0c8
Šarūnas Navickas <sarunas.navickas@tokenmill.lt>
2018-04-22 22:32:08 +0300

    Don't use pip deps

--- setup.py
+++ setup.py
@@ -2,15 +2,7 @@
 # -*- coding: UTF-8 -*-
 from setuptools import setup
 
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
-
 from oshino_admin.version import get_version
-
-install_reqs = list(parse_requirements("requirements/release.txt", session={}))
-test_reqs = list(parse_requirements("requirements/test.txt", session={}))
 
 setup(name="oshino-admin",
       version=get_version(),
@@ -18,11 +10,22 @@ setup(name="oshino-admin",
       author="zaibacu",
       author_email="zaibacu@gmail.com",
       packages=["oshino_admin"],
-      install_requires=[str(ir.req) for ir in install_reqs],
+      install_requires=[
+        "click==6.7",
+        "requests",
+        "daemonize"
+      ],
       test_suite="pytest",
-      tests_require=[str(tr.req) for tr in test_reqs],
+      tests_require=[
+         "pytest",
+         "coverage",
+         "coveralls",
+         "py"
+      ],
       setup_requires=["pytest-runner"],
-      entry_points={'console_scripts': ['oshino-admin = oshino_admin.run:main'
-                                        ]
-                    }
+      entry_points={
+          "console_scripts": [
+              "oshino-admin = oshino_admin.run:main"
+            ]
+      }
 )
